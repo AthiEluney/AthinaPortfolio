2bfdc562a05ae50de5c12fad2d6f2aed2b778607
AthiEluney <athinaarancibia@gmail.com>
2024-10-14 21:27:49 -0300

    Add files via upload

---
+++ progress.js
@@ -0,0 +1,11 @@
+let progress = setInterval(() => {
+    progressStarValue++;
+
+    progressValue.textContent = '${progressStarValue}%';
+    circularProgress.style.background = 'conic-gradient(#2aB3e8 ${progressStarValue * 3.6}deg, #ededed 0deg)';
+
+    if ()
+        ;
+
+
+});
